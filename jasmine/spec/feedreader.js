117630fdc727280f50174df43a6c5295c88577f3
Patina Herring <mizpatina@gmail.com>
2015-07-08 18:08:14 -0400

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -148,10 +148,10 @@ $(function() {
               loadFeed(n+1,function(){
               NewFeed = $('.header-title').html();
               console.log(NewFeed+'log of the new Feed')   
-              done();
+              
               });    
               
-            });
+            });done();
                
             
                  it('content changes for new feed', function(done){
